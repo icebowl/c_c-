636ae69e6e1c323aa6169b402cca9e267a37a05c
icebowl <craigcoleman.cs@gmail.com>
2019-02-01 15:09:08 -0800

    AA

---
+++ hello.c
@@ -0,0 +1,9 @@
+// hello.c cwc
+// compile gcc hello.c -o hello.o
+
+#include <stdio.h>
+int main()
+{
+   printf("Hello, World!");
+   return 0;
+}
---
+++ squareroot.c
@@ -0,0 +1,12 @@
+ /* sqrt example */
+  #include <stdio.h>
+  #include <math.h>
+
+  int main ()
+  {
+    double param, result;
+    param = 1024.0;
+    result = sqrt (param);
+    printf ("sqrt(%lf) = %lf\n", param, result );
+    return 0;
+ }
